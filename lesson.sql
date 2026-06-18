341c7bdfde53f3022415a30f7f52bde3561c12e4
renncart-cell <renncart@googlemail.com>
2026-06-18 11:23:54 +0000

    Refactor SQL query to specify selected columns and add WHERE clause for filtering

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,24 @@
-/*
-Example lesson stub
-*/
+\.
+
+
+
+
 
 SELECT
-	*
+	ps.PatientId
+    
+	,ps.Hospital
+    
+	,ps.Ward
+    
+	,DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
+    
+	,ps.AdmittedDate
+   -- , ps.DischargeDate
+    
+	,ps.Tariff
 FROM
-	PatientStay ps ;
+	PatientStay ps
+WHERE ps.Hospital IN ('Kingston', 'PRUH')
+	AND ps.Ward LIKE '%Surgery'
+	AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-02-29'
